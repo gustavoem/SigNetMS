563d58daac961dcd56bf7a388b681d0bd695dd8f
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-08-24 10:32:50 -0300

    ODES class can correctly store the initial state of every variable.

--- src/ODES.py
+++ src/ODES.py
@@ -5,24 +5,31 @@ class ODES:
     def __init__ (self):
         """ Default constructor. """
         self.index_map = {}
-        pass
+        self.rate_eq = {}
+        self.initial_state = []
 
     
     def add_equation (self, var, string):
         """ Adds an equation representing the change rate of a variable.
         """
-        pass
+        if var in self.index_map:
+            self.rate_eq[var] = string
+            return
+        
+        idx = len (self.index_map)
+        self.index_map[var] = idx
+        self.initial_state.append (None)
 
 
     def define_initial_value (self, var, value):
         """ Defines the initial value of a variable. """
-        pass
+        idx = self.index_map[var]
+        self.initial_state[idx] = value
 
 
     def evaluate_on (self, time_points):
         """ Returns the state of the systems variables at the specified
             time points. """
-        pass
-
+        return self.initial_state
 
 

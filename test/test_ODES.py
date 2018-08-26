04cf7405a7582b8b4af27c25b3e3c47f5ecd5a34
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-08-25 21:22:40 -0300

    Added a new system to be tested on ODE class. Still passing.

--- test/test_ODES.py
+++ test/test_ODES.py
@@ -26,14 +26,29 @@ class TestSBMLMethods (unittest.TestCase):
         """ Tests if the system can be solved numerically. """
         odes = ODES ()
         # dx1 (t)/dt = x1 (t)
+        # Solution is x1 (t) = exp (t)
         odes.add_equation ("x1", "x1")
         odes.define_initial_value ("x1", 1.0)
-        # Solution is x1 (t) = exp {t}
         t = np.linspace (0, 2, 11)
         y = odes.evaluate_on (t)
         for i in range (len (t)):
             analytic = math.exp (t[i])
             assert (abs (y[i] - analytic) < 1e-2)
+
+        odes = ODES ()
+        # dx1 (t)/dt = x1 (t)/2 + x2(t)/2
+        # dx2 (t)/dt = x1 (t)/2 + x2(t)/2
+        # Solution is x1 (t) = exp (t), x2 (t) = exp (t)
+        odes.add_equation ("x1", "x1/2 + x2/2")
+        odes.add_equation ("x2", "x1/2 + x2/2")
+        odes.define_initial_value ("x1", 1.0)
+        odes.define_initial_value ("x2", 1.0)
+        y = odes.evaluate_on (t)
+        for i in range (len (t)):
+            analytic = math.exp (t[i])
+            assert (abs (y[i][0] - analytic) < 1)
+            assert (abs (y[i][1] - analytic) < 1)
+
 
 
 if __name__ == '__main__':

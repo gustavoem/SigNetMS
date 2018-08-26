efe018f3f73d6bb2538ea6efa6c6f09bb2876ab9
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-08-25 21:09:17 -0300

    Implemented ODES class. It succesfully passed on the tests.

--- src/ODES.py
+++ src/ODES.py
@@ -1,3 +1,7 @@
+import re
+
+from scipy.integrate import odeint
+
 class ODES:
     """ This class contains a representation for systems of ordinary
         differential equations. """
@@ -5,19 +9,20 @@ class ODES:
     def __init__ (self):
         """ Default constructor. """
         self.index_map = {}
-        self.rate_eq = {}
+        self.rate_eq = []
         self.initial_state = []
 
     
-    def add_equation (self, var, string):
+    def add_equation (self, var, formula):
         """ Adds an equation representing the change rate of a variable.
         """
         if var in self.index_map:
-            self.rate_eq[var] = string
+            self.rate_eq[index_map[var]] = formula
             return
         
         idx = len (self.index_map)
         self.index_map[var] = idx
+        self.rate_eq.append (formula)
         self.initial_state.append (None)
 
 
@@ -30,6 +35,44 @@ class ODES:
     def evaluate_on (self, time_points):
         """ Returns the state of the systems variables at the specified
             time points. """
-        return self.initial_state
+        sys_function = self.__create_system_function ()
+        y = odeint (sys_function, self.initial_state, time_points)
+        return y
+
+    
+    # possible speedup: call this function only when it's necessary
+    # to redefine the system
+    def __create_system_function (self):
+        """ Creates a function that describes the dynamics of the 
+            system. """
+
+        evaluable_formulas = []
+        i = 0
+        for formula in self.rate_eq:
+            formula = re.sub (r'(([A-z]|_)\w*)', 
+                    lambda m: "current_state['" + m.group (0) + "']", 
+                    formula)
+            evaluable_formulas.append (formula)
+            i += 1
 
 
+        def system_function (state, t):
+            dstatedt = []
+            current_state = {}
+
+            for var in self.index_map:
+                idx = self.index_map[var]
+                current_state[var] = state[idx]
+
+            for i in range (len (state)):
+                x = 0
+                try:
+                    x = eval (evaluable_formulas[i])
+                except:
+                    print ("Couldn't evaluate system rate formula. " +
+                           "Did you define system variables " + 
+                           "correctly?")
+                dstatedt.append (x)
+            return dstatedt
+
+        return system_function
--- test/test_ODES.py
+++ test/test_ODES.py
@@ -19,7 +19,7 @@ class TestSBMLMethods (unittest.TestCase):
         odes.define_initial_value ("x1", 100.0)
         odes.define_initial_value ("x2", 250.5)
         y = odes.evaluate_on ([0])
-        self.assertEqual (y, [100, 250.5])
+        self.assertListEqual (list (y[0]), [100, 250.5])
 
 
     def test_integration (self):
@@ -28,11 +28,11 @@ class TestSBMLMethods (unittest.TestCase):
         # dx1 (t)/dt = x1 (t)
         odes.add_equation ("x1", "x1")
         odes.define_initial_value ("x1", 1.0)
-        # Solution is x1 (t) = exp {t^2 / 2}
+        # Solution is x1 (t) = exp {t}
         t = np.linspace (0, 2, 11)
         y = odes.evaluate_on (t)
         for i in range (len (t)):
-            analytic = math.exp (t[i] ** 2 / 2)
+            analytic = math.exp (t[i])
             assert (abs (y[i] - analytic) < 1e-2)
 
 

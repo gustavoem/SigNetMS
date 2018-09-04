23961c20f3d41b844c3f9b5d4e63a8fb30f63786
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-09-04 13:05:44 -0300

    ExperimentReader passed in all tests.

--- src/ExperimentReader.py
+++ src/ExperimentReader.py
@@ -1,4 +1,38 @@
-def read_experiment_file (file_name):
+from Experiment import Experiment
+
+def read_experiment_file (file_name, var):
     """ This function can read experiment files. The experiment data is 
         organized in a list of Experiment objects. """
-    return None
+    f = open (file_name)
+    experiment_values = []
+    times = []
+    experiments = []
+    for line in f:
+        l = line.split ()
+        # remove comments
+        if '#' in l:
+            l = l[0:l.index ('#')]
+        if len (l) == 0:
+            # create experiment objects
+            for exp_val in experiment_values:
+                exp = Experiment (times, exp_val, var)
+                experiments.append (exp)
+                experiment_values = []
+                times = []
+            continue
+        
+        times.append (l[0])
+        if (experiment_values == []):
+            nof_exp_per_line = len (l) - 1
+            experiment_values = [[] for i in range (len (l) - 1)]
+        for i in range (1, len (l)):
+            experiment_values[i - 1].append (l[i])
+
+    for exp_val in experiment_values:
+        exp = Experiment (times, exp_val, var)
+        experiments.append (exp)
+        experiment_values = []
+        times = []
+
+    f.close ()
+    return experiments
--- test/test_ExperimentReader.py
+++ test/test_ExperimentReader.py
@@ -9,7 +9,7 @@ class TestExperimentReader (unittest.TestCase):
 
     def test_read_experiment (self):
         """ Tests if the module can read an experiment """
-        data = read_experiment_file ('input/ERK_data.txt')
+        data = read_experiment_file ('input/ERK_data.txt', 'ERK')
         self.assertEqual (len (data), 25)
         exp0_data = data[0]
         self.assertEqual (len (exp0_data.times), 6)

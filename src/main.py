86653c93505ac757ecf3e0bf3e8a30cc45b7b569
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-09-27 11:19:00 -0300

    Added goodwin3.data.

--- src/main.py
+++ src/main.py
@@ -2,20 +2,25 @@ from SBML import SBML
 from ODES import ODES
 from SBMLtoODES import sbml_to_odes
 from Experiment import Experiment
-from ExperimentReader import read_experiment_file 
+from ExperimentReader import read_txt_experiment_file 
+from ExperimentReader import read_data_experiment_file 
 import MarginalLikelihood as ml
 import numpy as np
 
-sbml = SBML ()
-sbml.load_file ('../input/model1.xml')
-# sbml.load_file ('../input/goodwin3.xml')
-odes = sbml_to_odes (sbml)
+goodwin_experiment = False
 
-params = odes.get_all_parameters ()
-for p in params:
-    print (p)
 
-experiments = read_experiment_file ('../input/ERK_small.txt', 'ERK')
-# odes.overtime_plot ("ERK", experiments[0].times)
-# odes.overtime_plot ("x1", np.linspace (0, 1200, num=100))
-# ml.estimate_marginal_likelihood (experiments, sbml, odes)
+if goodwin_experiment:
+    sbml = SBML ()
+    sbml.load_file ('../input/goodwin3.xml')
+    odes = sbml_to_odes (sbml)
+    experiments = read_data_experiment_file ('../input/goodwin3.data', 
+            'x1')
+    ml.estimate_marginal_likelihood (experiments, sbml, odes)
+else:
+    sbml = SBML ()
+    sbml.load_file ('../input/model1.xml')
+    odes = sbml_to_odes (sbml)
+    experiments = read_txt_experiment_file ('../input/ERK_data.txt', 
+            'ERK')
+    ml.estimate_marginal_likelihood (experiments, sbml, odes)

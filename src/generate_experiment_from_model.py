7c53b2a949a170217f4bc2985eed9a4fb7c25dd3
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-10-05 09:15:30 -0300

    Now it is possible to create syntetic data.

--- src/generate_experiment_from_model.py
+++ src/generate_experiment_from_model.py
@@ -7,8 +7,9 @@ import numpy as np
 
 def add_noise (values):
     for i in range (len (values)):
-        eps = np.random.normal (0, 1)
-        values[i] += eps
+        eps = np.random.normal (0, .1)
+        if values[i] + eps > 0:
+            values[i] += eps
 
 
 sbml = SBML ()
@@ -18,4 +19,4 @@ time = np.linspace (0, 200, 201)
 values = odes.evaluate_on (time)
 add_noise (values["E"])
 experiment = Experiment (time, values["E"], "x1")
-experiment.save_to_file ('exp_file.data')
+experiment.save_to_file ('../input/simple_enzymatic.data')

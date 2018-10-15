643f5c8cc4cf5e58a0767bad19fb3f6ed3bc98b6
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-10-15 11:00:25 -0300

    Implemented covariance matrix estimator.

---
+++ src/CovarianceMatrix.py
@@ -0,0 +1,24 @@
+# This modules defines a function that calculates the covariance matrix
+# of variables given samples.
+
+import numpy as np
+
+def calc_covariance (sample):
+    """ Given a list of samples (lists of values) of variables, compute
+        an estimation of the covariance matrix. """
+    n = len (sample[0])
+    theta_sum = np.zeros (n)
+    for theta in sample:
+        theta_sum += theta
+    theta_mean = theta_sum / len (sample)
+       
+    cov_matrix = np.zeros ([n, n])
+    for j in range (len (sample)):
+        thetaj = sample[j]
+        v = thetaj - theta_mean
+        v = v.reshape ([n, 1])
+        vT = np.array (v.transpose ())
+        cov_matrix += v * vT
+    return cov_matrix / len (sample)
+
+     

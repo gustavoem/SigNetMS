Minor fix. The variance of X_i when X ~ MultivariateNormal (0, \Sigma) is \Sigma_{i, i}.

src/AdaptiveMCMC.py
@@ -55,7 +55,7 @@ class AdaptiveMCMC:
         for i in range (len (jump)):
             # To make a normal random variable lognormal you should
             # exponentiate it
-            lognorm_mean = np.exp (cov[i][i] * cov[i][i] / 2)
+            lognorm_mean = np.exp (cov[i][i] / 2)
             ith_jump = np.exp (jump[i]) - lognorm_mean 
             new_value = ith_jump + new_t[i].value
             if new_value > 0 and new_value < float ('inf'):
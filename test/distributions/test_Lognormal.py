4090ae1c3c9b86927699f0af97a3f5975b03f5fa
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-11-22 15:01:18 -0200

    Created tests for Lognormal distribution.

--- test/distributions/test_Lognormal.py
+++ test/distributions/test_Lognormal.py
@@ -12,7 +12,29 @@ class TestLognormal (unittest.TestCase):
         mu = 2
         s = 2
         X = Lognormal (mu, s)
-        self.assertEqual (X.mean (), np.exp (mu + s * s / 2))
+        assert (abs (X.mean () - np.exp (mu + s * s / 2)) < 1e-4)
 
-    def test_fail (self):
-        self.assertEqual (1, 2)
+
+    def test_get_random_value (self):
+        """ Tests if one can get the a random value from the random
+            variable. """
+        mu = -100
+        s = 1
+        X = Lognormal (mu, s)
+        assert (X.rvs () > 0)
+
+
+    def test_get_pdf (self):
+        """ Tests if we can get a point of the probability density
+            function of this random variable. """
+        mu = 1
+        s = 1
+        X = Lognormal (mu, s)
+        x = 2
+        # If the underlying Normal distribution is N (mu, s²) then,
+        # the pdf of X is f(x):
+        # 1/sqrt (2pi * s²) exp (-(ln(x) - mu)²/(2 * s²)) * 1/x
+        analytic = (1 / sqrt (2 * np.pi * s * s)) \
+                    * np.exp (-(np.log (x) - mu) ** 2 / (2 * s * s)) \
+                    * (1 / x)
+        assert (abs (X.pdf (x) - analytic) < 1e-4)

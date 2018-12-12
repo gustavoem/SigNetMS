3026a64c7b0f71facbf00dd7134fd42e3bdb73db
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-12-12 16:50:05 -0200

    Tested writing and reading an ExperimentSet.

--- test/test_ExperimentSet.py
+++ test/test_ExperimentSet.py
@@ -5,6 +5,8 @@ import os
 import unittest
 from ExperimentSet import ExperimentSet
 from Experiment import Experiment
+from ExperimentReader import read_data_experiment_file
+
 
 class TestExperimentSet (unittest.TestCase):
     
@@ -35,3 +37,25 @@ class TestExperimentSet (unittest.TestCase):
         assert all ([m in data for m in measures])
         assert all ([str (v) in data for v in values])
 
+
+    def test_write_then_read (self):
+        """ Tests if one can save experiments to a file and then
+            read. """
+        exp1 = Experiment ([1, 2, 3, 4], [.1, .2, .3, .4], 'x1')
+        exp2 = Experiment ([1, 3, 9, 27], [.1, .2, .3, .4], 'x2')
+        exp_set = ExperimentSet ()
+        exp_set.add (exp1)
+        exp_set.add (exp2)
+        out_file = 'tmp_exp_set_file.xml'
+        exp_set.save_to_file (out_file)
+        read_exp_set = read_data_experiment_file (out_file)
+        self.assertEqual (read_exp_set.get_size (), 2)
+        exp0 = read_exp_set[0]
+        exp1 = read_exp_set[1]
+        self.assertListEqual (list (exp0.times), [1, 2, 3, 4])
+        self.assertListEqual (list (exp0.values), [.1, .2, .3, .4])
+        self.assertEqual (exp0.measure_expression, 'x1')
+        self.assertListEqual (list (exp1.times), [1, 3, 9, 27])
+        self.assertListEqual (list (exp1.values), [.1, .2, .3, .4])
+        self.assertEqual (exp1.measure_expression, 'x2')
+        os.remove (out_file)

798723ea7e4414ccfccf1f69f6bab03131bae621
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-12-12 18:11:07 -0200

    Updated bioinformatics experiments files.

---
+++ input/bioinformatics/generate_experiment_from_model.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.insert (0, '../../src/')
+
+from SBML import SBML
+from ODES import ODES
+from SBMLtoODES import sbml_to_odes
+from ExperimentSet import ExperimentSet
+from Experiment import Experiment
+import numpy as np
+
+
+def add_noise (values):
+    for i in range (len (values)):
+        eps = np.random.normal (0, .01)
+        if values[i] + eps > 0:
+            values[i] += eps
+
+sbml = SBML ()
+sbml.load_file ('model1.xml')
+odes = sbml_to_odes (sbml)
+time = [0, 2, 5, 10, 20, 40, 60, 100]
+values = odes.evaluate_on (time)
+
+experiment_set = ExperimentSet ()
+for i in range (3):
+    noised_values = {}
+    for x in values:
+        noised_values[x] = list (values[x])
+
+    add_noise (noised_values["Rpp"])
+    experiment = Experiment (time[1:], noised_values["Rpp"][1:], 
+            "Rpp")
+    experiment_set.add (experiment)
+
+experiment_set.save_to_file ('experiment.data')

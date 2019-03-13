Minor fix and added variance method on Lognormal distribution.

distributions/Lognormal.py
@@ -13,7 +13,7 @@ class Lognormal:
     
     def copy (self):
         """ Returns a copy of this object. """
-        cpy = Lognormal (self.__mu, self.__sigma)
+        cpy = Lognormal (self.__mu, self.__s)
         return cpy
         
 
@@ -24,6 +24,11 @@ class Lognormal:
         s = self.__s
         return np.exp (mu + s * s / 2)
 
+    def variance (self):
+        """ Returns the variance of the Lognormal distribution. """
+        s2 = self.__s * self.__s
+        var = (np.exp (s2) - 1) * np.exp (2 * self.__mu + s2)
+        return var
 
     def rvs (self, n=None):
         """ Returns independent observations of this random variable. 
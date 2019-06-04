852655224223b55a4f13e95d877ad4e841bfc712
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2019-06-04 17:14:54 -0300

    Added time experiment script.

---
+++ time_experiment.sh
@@ -0,0 +1,1 @@
+perf stat -r 10 python3 SigNetMS.py input/simple_enzymatic/simple_enzymatic.xml input/simple_enzymatic/simple_enzymatic.priors input/simple_enzymatic/simple_enzymatic.data 200 50 20 20

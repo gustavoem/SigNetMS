7458133bb8fc53b852ee7f95683f79cff74f8b4a
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2019-06-04 17:12:14 -0300

    Minor fix on parallel map.

--- parallel_map.py
+++ parallel_map.py
@@ -9,26 +9,34 @@ def __fun_runner (f, q_in, q_out):
         i, x = q_in.get ()
         if i is None:
             break
-        q_out.put ((i, f (x)))
+        run_result = (i, f (x))
+        q_out.put (run_result)
 
 
 def parallel_map (f, X, nof_process):
     """ Runs a map of X to f in parallel, using nof_process process. """
     q_in = multiprocessing.Queue (1)
     q_out = multiprocessing.Queue ()
-    
+
     proc = [multiprocessing.Process (target=__fun_runner, 
         args=(f, q_in, q_out)) for _ in range (nof_process)]
 
     for p in proc:
         p.daemon = True
         p.start ()
+    
+    sent = 0
+    for i, x in enumerate (X):
+        q_in.put ((i, x))
+        sent += 1
 
-    sent = [q_in.put ((i, x)) for i, x in enumerate (X)]
     # sends a final signal to every worker
     for _ in range (nof_process):
-        q_in.put ((None, None))
+        q_in.put ((None, None), block=True)
+    q_in.close ()
+
+    res = [q_out.get () for _ in range (sent)]
+
     for p in proc:
         p.join ()
-    res = [q_out.get () for _ in range (len (sent))]
     return [x for i, x in sorted (res)]

f24d127a2e6a2440af8a8a4bf425c73698c01131
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2019-06-10 19:32:05 -0300

    Added an error raise and a warning when parameter priors do not match exactly model parameters.

--- model/PriorsReader.py
+++ model/PriorsReader.py
@@ -5,6 +5,7 @@ from distributions.Lognormal import Lognormal
 from utils import clean_tag
 from lxml import etree
 import sys
+import warnings
 
 def __create_distribution (dist_type, args):
     """ Creates a distribution dist_type with arguments args. 
@@ -58,21 +59,29 @@ def define_sbml_params_priors (sbml, filename):
     """ Reads the priors of all sbml parameters defined in filename. """
     default_priors = read_priors_file (filename)
     priors = RandomParameterList ()
-
+    original_names = []
     for param in sbml.get_all_param ():
         original_name = sbml.get_original_param_name (param)
+        original_names.append (original_name)
         param_prior = None
         for prior_p in default_priors.get_model_parameters ():
             if original_name == prior_p.name:
                 param_prior = prior_p.copy ()
                 param_prior.name = param
                 break
-        
+
         if param_prior == None:
-            sys.exit ("ERROR! Could not find a prior for " + \
+            raise ValueError ("Could not find a prior for " + \
                     original_name + " in priors defined in " + filename)
         
         priors.append (param_prior)
+        
+
+    for prior in default_priors.get_model_parameters ():
+        if prior.name not in original_names:
+            warnings.warn ("Prior of " + prior.name + " do not " + \
+                    "correspond to any of the model parameters.")
+
     sigma_p = default_priors.get_experimental_error_distribution ()
     priors.set_experimental_error (sigma_p) 
     return priors

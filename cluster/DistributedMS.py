66c3c35a07b59e07ae3897f691df94f0dd711dce
Gustavo Estrela <estrela.gustavo.matos@gmail.com>
2019-07-23 15:45:37 +0000

    Now it is possible to create the redis service in a head node.

--- cluster/DistributedMS.py
+++ cluster/DistributedMS.py
@@ -1,10 +1,28 @@
 import argparse
 import json
-import os
+import subprocess 
+import re
+from pathlib import Path
 
 def prepare_workers (workers, ray_path):
-    # start the first element as a host
-    os.sys ("start_cluster_head.sh " + workers[0] + ray_path)
+    # start the first worker as a host
+    current_path = str (Path ().absolute ())
+    start_cluster_head_bin = current_path + "/start_cluster_head.sh"
+    proc = subprocess.Popen ([start_cluster_head_bin, workers[0], \
+            ray_path], stdout=subprocess.PIPE)
+    (out, err) = proc.communicate ()
+    # print (out.decode ("utf-8"))
+    redis_address_pattern = \
+            re.compile (r".*redis-address (\d+\.\d+\.\d+\.\d+:\d+)")
+    m = redis_address_pattern.match (str (out))
+    redis_address = m.group (1)
+
+
+    
+    
+    for worker in workers[1:]:
+        # start other worker as a normal worker
+        pass
 
 
 parser = argparse.ArgumentParser ()
--- cluster/start_cluster_head.sh
+++ cluster/start_cluster_head.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
+
 head=$1
 ray_path=$2
 
+echo "ssh $head $ray_path start --head 2>&1 > /dev/null"
 ray_out=$(ssh $head $ray_path start --head 2>&1 >/dev/null)
 echo "$ray_out"
 
